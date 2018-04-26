2794946364478e86c081338aaec2610256508df8
DarryNOBI <darryfei@gmail.com>
2018-04-26 19:42:36 +0800

    Merge branch 'master' of https://github.com/DarryNobi/Demo
    # Conflicts:
    #	web/views.py

--- static/js/default_geomap.js
+++
@@ -1,5 +0,0 @@
-
-       var default_geo_layer = new ol.layer.Tile({
-            source: new ol.source.OSM(),
-            opacity:0.2,
-        });
--- static/js/geomap.js
+++ static/js/geomap.js
@@ -1,21 +1,28 @@
 
         var geoserver_layer = new ol.layer.Image({
-              source: new ol.source.ImageWMS({
-              url:'http://172.20.53.157:8080/geoserver/wms',
-              projection:'EPSG:4326',
-              params:{
+            source: new ol.source.ImageWMS({
+                url:'http://172.20.53.157:8080/geoserver/wms',
+                projection:'EPSG:4326',
+                params:{
                 LAYERS: 'test:resample'}
-              }),
-              opacity:0.5,
-            });
+            }),
+            projection: "EPSG:4326",
+            opacity:0.5,
+        });
 
         //172.20.53.157:8080/geoserver/wms
 
-        var geoserver_layer2=new ol.layer.Tile({
-                source: new ol.source.XYZ({
-                    url:'http://t3.tianditu.cn/DataServer?T=img_w&X={x}&Y={y}&L={z}'
-                }),
-                projection: "EPSG:4326",
-                id:"影像",
-                opacity:1,
-            });
+        var default_geo_layer2=new ol.layer.Tile({
+            source: new ol.source.XYZ({
+                url:'http://t3.tianditu.cn/DataServer?T=img_w&X={x}&Y={y}&L={z}'
+            }),
+            projection: "EPSG:4326",
+            id:"影像",
+            opacity:1,
+        });
+
+        var default_geo_layer = new ol.layer.Tile({
+            source: new ol.source.OSM(),
+            projection: "EPSG:4326",
+            opacity:0.2,
+        });
