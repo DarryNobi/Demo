730c7b0dbc4af37ebc586773056af4dcf23c8fb2
DarryNobi <darryfei@gmail.com>
2018-04-27 17:46:55 +0800

    Merge branch 'master' of https://github.com/DarryNobi/Demo
    # Conflicts:
    #	templates/authorityManagement.html
    #	web/views.py

--- static/js/test.js
+++ static/js/test.js
@@ -1,39 +1,65 @@
 function showList(){
-    var graphicTab = $("#graphic_tab");
-    data.forEach(function(item){
-        $(
-            '<tr/>',
-                {
-            }).append($('<td/>', {
-                text : item.id
-            }))
-            .append($('<td/>',{
-                text : item.name
-            }))
-            .append($('<td/>',{
-                text : item[site_type]
-            }))
-            .append($('<td/>',{
-                text : item[sign_type]
-            }))
-            .append($('<td/>',{
-                text : item.area
-            }))
-            .append($('<td/>',{
-                text : item[longi_lati_tude]
-            }))
-            .append($('<td/>',{
-                text : item.description
-            }))
-            .append($('<td/>')
-            .append($('<p/>')
-            .append($('<button/>',{
-                'class' : 'operate',
-                text : '修改'
-            }))
-            .append($('<button/>',{
-                'class' : 'operate',
-                text : '删除'
-            }))))
-            .appendTo(graphicTab);
-    });
+        var resultTab = $("#resultTab");
+        data.forEach(function(item){
+            $(
+                '<tr/>', {
+                    'style' : 'font-size:18px'
+                }).append($('<td/>', {
+                    text : item.id
+                }))
+                .append($('<td/>',{
+                    text : item.resource
+                }))
+                .append($('<td/>',{
+                    text : item.date
+                }))
+                .append($('<td/>',{
+                    text : item.user
+                }))
+                .append($('<td/>',{
+                    text : item.load
+                }))
+                .append($('<td/>',{
+                    text : item.satellite
+                }))
+                .append($('<td/>',{
+                    text : item.type
+                }))
+                .append($('<td/>',{
+                    text : item.download
+                }))
+                .append($('<td/>')
+                .append($('<p/>')
+                .append($('<button/>',{
+                    'class' : 'operate',
+                    'id' : 'look' + item.id,
+                    text : '查看'
+                }))
+                .append($('<button/>',{
+                    'class' : 'operate',
+                    'id' : 'download' + item.id,
+                    text : '下载'
+                }))
+                .append($('<button/>',{
+                    'class' : 'operate',
+                    'id' : 'del' + item.id,
+                    text : '删除'
+                }))
+                .append($('<button/>',{
+                    'class' : 'operate',
+                    'id' : 'release' + item.id,
+                    text : '发布'
+                }))
+                .append($('<button/>',{
+                    'class' : 'operate',
+                    'id' : 'cancel_release' + item.id,
+                    text : '取消发布'
+                }))))
+                .appendTo(resultTab);
+                $("#look"+item.id).on("click", look);
+                $("#download"+item.id).on("click", download);
+                $("#del"+item.id).on("click", del);
+                $("#release"+item.id).on("click", release);
+                $("#cancel_release"+item.id).on("click", cancel_release);
+        });
+    };
